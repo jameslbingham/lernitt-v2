39178cb247bf4be2ac62c2fe9c1355bfea45d885
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2026-01-10 12:56:28 +1100

    Update mongodb.ts

--- src/lib/database/mongodb.ts
+++ src/lib/database/mongodb.ts
@@ -1,27 +1,24 @@
+// @ts-nocheck
 import { MongoClient } from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
 
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MONGODB_URI to your .env.local file');
-}
-
 if (process.env.NODE_ENV === 'development') {
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
-
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri!, options);
-    globalWithMongo._mongoClientPromise = client.connect();
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri!, options);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
