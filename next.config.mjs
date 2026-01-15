474a364d68dbcf2d3180e74378d41e1319d82b80
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2026-01-15 14:33:24 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false, // Disables the modern minifier
-  experimental: {
-    forceSwcTransforms: false, // Stops the specific binary load
-  }
+  swcMinify: false,
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  },
 };
 export default nextConfig;
