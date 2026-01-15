d4662ddedcf5283cac360e53cb9f35c209c559e6
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2026-01-15 13:52:59 +1100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Disables the modern compiler that crashes older Mac hardware
+  swcMinify: false,
+};
+
+export default nextConfig;
