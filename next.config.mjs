32a59c3d14e33b02cf0f794290e1ba6295aaece2
jameslbingham <104026749+jameslbingham@users.noreply.github.com>
2026-01-15 14:07:08 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Forces Next.js to use a compiler compatible with your Intel Mac
   swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+  },
 };
 
 export default nextConfig;
